4d4c7232f2be2b28bf1349bb231d92257102749f
Rafael Fischer <rafaelfischer@gmail.com>
2019-01-31 11:20:08 -0200

    Initial commit

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro',
+  templateUrl: './meu-primeiro.component.html',
+  styleUrls: ['./meu-primeiro.component.css']
+})
+export class MeuPrimeiroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
